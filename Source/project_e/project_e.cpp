23b57d6395531bdec639b2f4987ff8ef039f9523
Igor Kononov <115949454+fsgkunreal@users.noreply.github.com>
2026-06-15 20:46:03 +0300

    c++ sourse code

---
+++ Source/project_e/project_e.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "project_e.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, project_e, "project_e" );
