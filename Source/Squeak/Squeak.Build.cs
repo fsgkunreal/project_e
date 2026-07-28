847d858e5cb56009e47bef06ccc8d196cd946e0c
Igor Kononov <115949454+fsgkunreal@users.noreply.github.com>
2026-07-28 11:22:04 +0300

    AdvancedSessionsUpload

--- Source/Squeak/Squeak.Build.cs
+++ Source/Squeak/Squeak.Build.cs
@@ -8,7 +8,7 @@ public class Squeak : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "SteamShared", "SteamSdk", "OnlineSubsystemSteam" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
